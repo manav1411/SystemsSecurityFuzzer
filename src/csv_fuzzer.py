917af35dc7266dfc9a8e2f9ccac2570a1cd3069b
Jerry M Yang <jmax.yang@gmail.com>
2024-10-25 12:08:33 +1100

    Added CSV to List and Vice Versa Functions

--- src/csv_fuzzer.py
+++ src/csv_fuzzer.py
@@ -7,20 +7,6 @@ from utils import print_crash_found, print_no_crash_found, get_process, write_cr
 # Number of Total Mutations
 NUM_MUTATIONS = 3
 
-# taken from json_fuzzer.py, but not used
-MASS_POS_NUM = 999999999999999999999999999999999999999999999999999999
-MASS_NEG_NUM = -999999999999999999999999999999999999999999999999999999
-OVERFLOW = "A" * 10000
-BOUNDARY_MINUS = -1
-BOUNDARY_PLUS = 1
-ZERO = 0
-ONE_BYTE = 128
-TWO_BYTE = 32768
-FOUR_BYTE = 2147483648
-EIGHT_BYTE = 9223372036854775808
-FORMAT = "%p"
-
-
 def is_csv(words):
     try:
         csv.reader(words)
@@ -28,25 +14,50 @@ def is_csv(words):
         return False
     return True
 
-def csv_to_string(words):
-    output = io.StringIO()
-    writer = csv.writer(output)
+'''
+Creates an array of arrays representing each row
+E.g.
+header,must,stay,intact
+a,b,c,S
+e,f,g,ecr
+i,j,k,et
 
-    reader = csv.reader(words)
+-> 
+[['header', 'must', 'stay', 'intact'], ['a', 'b', 'c', 'S'], ['e', 'f', 'g', 'ecr'], ['i', 'j', 'k', 'et']]
+'''
+def csv_to_list(data):
+    list = []
+    f = io.StringIO(data)
+    for row in csv.reader(f, delimiter=','):
+        list.append(row)
+    return list
 
-    for row in reader:
-        writer.writerow(row)
+'''
+Does the opposite of above
+E.g.
+[['header', 'must', 'stay', 'intact'], ['a', 'b', 'c', 'S'], ['e', 'f', 'g', 'ecr'], ['i', 'j', 'k', 'et']]
+->
+header,must,stay,intact
+a,b,c,S
+e,f,g,ecr
+i,j,k,et
 
-    csv_string = output.getvalue()
-    output.close()
-    return csv_string
+'''
+def list_to_csv(data):
+    csv = ''
+    for row in data:
+        newline = ''
+        for ele in row:
+            newline += f'{ele},'
+        csv += f'{newline[:-1]}\n'
+    return csv + '\n'
 
 '''
 Sends a given input to a process, then returns whether the process crashes or not
 '''
 def send_to_process(p, csv_payload, filepath):
-    payload = csv_to_string(csv_payload)
-    p.sendline(csv_to_string(payload).encode('utf-8'))
+    payload = ''
+    p.sendline(payload)
     p.proc.stdin.close()
 
     code = p.poll(True)
@@ -54,6 +65,40 @@ def send_to_process(p, csv_payload, filepath):
     if code != 0:
         write_crash_output(filepath, payload)
         return True
+    else:
+        return False
+
+'''
+Main function call to begin fuzzing CSV input binaries
+'''
+def fuzz_csv(filepath, words):
+    print(words)
+    csv_list = csv_to_list(words)
+    print(csv_list)
+    csv_back_to_csv = list_to_csv(csv_list)
+    print(csv_back_to_csv)
+
+    for i in range(0, NUM_MUTATIONS):
+        deepcopy = copy.deepcopy(csv_list)
+
+        if perform_mutation(filepath, deepcopy, i):
+            print_crash_found()
+            exit()
+
+    print_no_crash_found()
+
+'''
+Begins the mutation process with a range of CSV files
+'''
+def perform_mutation(filepath, data, i):
+    if i == 0:
+        print("> Testing Normal Payload")
+        return send_to_process(get_process(filepath), data, filepath)
+    elif i == 1:
+        print("> Testing Empty Payload")
+        return send_to_process(get_process(filepath), "", filepath)
+    elif i == 2:
+        return add_extra_comma(data, filepath)
     else:
         return False
 
@@ -82,35 +127,3 @@ def add_extra_comma(data: csv, filepath):
             return True
 
     return False
-
-
-'''
-Begins the mutation process with a range of CSV files
-'''
-def perform_mutation(filepath, data, i):
-    if i == 0:          # Default Payload Test
-        print("> Testing Normal Payload")
-        return send_to_process(get_process(filepath), data, filepath)
-    elif i == 1:
-        print("> Testing Empty Payload")
-        return send_to_process(get_process(filepath), "", filepath)
-    elif i == 2:
-        return add_extra_comma(data, filepath)
-    else:
-        return False
-
-
-'''
-Main function call to begin fuzzing CSV input binaries
-'''
-def fuzz_csv(filepath, words):
-    csv_string = csv_to_string(words)
-
-    for i in range(0, NUM_MUTATIONS):
-        deepcopy = copy.deepcopy(csv_string)
-
-        if perform_mutation(filepath, deepcopy, i):
-            print_crash_found()
-            exit()
-
-    print_no_crash_found()
